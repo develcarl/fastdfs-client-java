1e9700b4a41d1e21d188f820531324c20e79e273
yichen <yichen-2@163.com>
2018-06-05 16:34:57 +0800

    修改autoconfiguration 保证并发上传不会报错

--- src/main/java/org/csource/autoconfig/FastDFSAutoConfiguration.java
+++ src/main/java/org/csource/autoconfig/FastDFSAutoConfiguration.java
@@ -4,15 +4,12 @@ import org.apache.log4j.Logger;
 import org.csource.fastdfs.ProtoCommon;
 import org.csource.fastdfs.StructGroupStat;
 import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
 
 /**
  * @author yichen
@@ -39,16 +36,4 @@ public class FastDFSAutoConfiguration {
         return new TrackerClient(fastDFSProperties);
     }
 
-    @Bean
-    @ConditionalOnMissingBean({TrackerServer.class})
-    public TrackerServer trackerServer(TrackerClient client){
-        TrackerServer server = null;
-        try {
-            server = client.getConnection();
-            ProtoCommon.activeTest(server.getSocket());
-        }catch (IOException e){
-            logger.error("get client connection failed!", e);
-        }
-        return server;
-    }
 }
